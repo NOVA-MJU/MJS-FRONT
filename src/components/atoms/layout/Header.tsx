fe5496b09da1e131682c9c54629d9fd092e9d226
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-06-24 18:59:16 +0900

    <Fix> Layout Header component 살짝꿍 조절

--- src/components/atoms/layout/Header.tsx
+++ src/components/atoms/layout/Header.tsx
@@ -8,18 +8,19 @@ export default function Header() {
       <div className='w-full max-w-screen-xl mx-auto flex gap-4'>
         <div className='flex flex-col gap-4 flex-2 w-full'>
           <div className='flex items-center gap-4 text-[1.1rem] font-semibold text-[#0055ff]'>
-            <FaBullhorn className='text-[1.4rem] ml-5' />
-            <span>현재 Version1 작업중입니다 _ MJS 일동</span>
+            <div className='flex gap-3 mt-5'>
+              <FaBullhorn className='text-[1.4rem] ' />
+              <span>현재 Version1 작업중입니다 _ MJS 일동</span>
+            </div>
           </div>
           <div>
-            <SearchBar className='ml-4' />
+            <SearchBar className='ml-4 mt-1' />
           </div>
         </div>
 
-        <div className='flex-1 w-full'>
+        <div className='flex-1 w-full mt-5'>
           <ProfileComponent className='mr-7' />
         </div>
-
       </div>
     </div>
   );
