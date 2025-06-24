28fc9de76b3c3f6ed39225997e9f88d944a62d99
yuraup <o721@naver.com>
2025-06-24 16:12:49 +0900

    <Feat> 로그인 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import ButtonTestPage from './components/atomic/button/ButtonTestPage';
-import InputTestPage from './components/atomic/input/InputTestPage';
-import Layout from './components/atomic/layout/Layout';
+import ButtonTestPage from './components/atoms/button/ButtonTestPage';
+import InputTestPage from './components/atoms/input/InputTestPage';
+import Layout from './components/layout/Layout';
+import LoginPage from './pages/LoginPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Layout></Layout>}>
-        {' '}
         <Route path='/test/button' element={<ButtonTestPage></ButtonTestPage>}></Route>
         <Route path='/test/input' element={<InputTestPage></InputTestPage>}></Route>
+        <Route path='/login' element={<LoginPage />}></Route>
       </Route>
     </Routes>
   );
