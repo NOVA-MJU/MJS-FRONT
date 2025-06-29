d62945a90b472eb787e6c4aba4b2cac0a03b7729
yuraup <o721@naver.com>
2025-06-29 21:43:18 +0900

    <Feat> 명대신문과 공지사항 리스트를 공통 컴포넌트로 통합

--- src/pages/notice/index.tsx
+++ src/pages/notice/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import CategoryFilter from '../../components/molecules/common/CategoryFilter';
 import { Notices } from '../../constants/notices';
 import { noticeData } from '../../constants/noticeData';
-import NoticeList from '../../components/organisms/NoticeList';
+import NoticeList from '../../components/organisms/CommonList';
 
 const Notice: React.FC = () => {
   const [page, setPage] = useState(1);
@@ -14,6 +14,12 @@ const Notice: React.FC = () => {
     selectedCategory === '전체'
       ? noticeData
       : noticeData.filter((notice) => notice.category === selectedCategory);
+
+  // pagination 관련 로직
+  const ITEMS_PER_PAGE = 8; // 한 페이지에 보여줄 아이템 수
+  const totalPages = Math.ceil(filteredNotices.length / ITEMS_PER_PAGE); //전체 페이지 수
+  const currentItems = filteredNotices.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE); // 현재 페이지에 보여줄 아이템 목록
+
   return (
     <div className='bg-grey-05 w-[1240px] min-h-screen flex flex-col p-12 gap-6 mx-auto'>
       <p className='text-4xl font-bold text-blue-900 text-mju-primary'>공지사항</p>
@@ -21,10 +27,13 @@ const Notice: React.FC = () => {
       <CategoryFilter
         categories={Notices}
         current={selectedCategory}
-        onChange={setSelectedCategory}
+        onChange={(category) => {
+          setSelectedCategory(category);
+          setPage(1);
+        }}
       />
-      <NoticeList key={selectedCategory} items={filteredNotices} />
-      <Pagination page={page} totalPages={10} onChange={(next) => setPage(next)} />
+      <NoticeList key={selectedCategory} items={currentItems} category={'notice'} />
+      <Pagination page={page} totalPages={totalPages} onChange={(next) => setPage(next)} />
     </div>
   );
 };
