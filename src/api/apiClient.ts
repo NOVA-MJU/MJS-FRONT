e9703bb8972531086588bcc23a971213359d2128
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-07-03 18:37:53 +0900

    <Test> Requested PR

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use((config) => {
   // const token = localStorage.getItem('accessToken');
   // 유라님 TODO이 여기서 토큰 handle 해주세요!
-  //env ignoring successFul
+  //env ignoring test
   // if (token) {
   //   config.headers.Authorization = `Bearer ${token}`;
   // }
