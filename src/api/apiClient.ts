40e09277247a5161089fb4755aaa405441daee63
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-07-03 19:31:22 +0900

    <Feat> After Rebase Test

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -12,7 +12,8 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use((config) => {
   // const token = localStorage.getItem('accessToken');
   // 유라님 TODO이 여기서 토큰 handle 해주세요!
-  //env ignoring test
+  //env ignoring test + rebase to basic branch
+  //after rebasing develop
   // if (token) {
   //   config.headers.Authorization = `Bearer ${token}`;
   // }
