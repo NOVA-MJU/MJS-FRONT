3cef306e601762c6ae1b9dc04b5fee89267289ac
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-07-07 19:42:35 +0900

    <Feat> 식단 API 연동

--- src/components/organisms/sections/NoticeSection.tsx
+++ src/components/organisms/sections/NoticeSection.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useEffect, useState } from 'react';
 import TabComponent from '../../molecules/mainpage/TabComponent';
 import { fetchNotionInfo } from '../../../api/notice/noticeApi';
