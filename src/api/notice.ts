5776f18faba5ce573baa3fd4520801ceb1afc19f
yuraup <o721@naver.com>
2025-07-07 20:50:16 +0900

    <Feat> 공지사항 페이지 api 연결 및 디자인과 일치하도록 수정

---
+++ src/api/notice.ts
@@ -0,0 +1,28 @@
+import apiClient from './apiClient';
+
+export interface NoticeQuery {
+  category: 'general' | 'academic' | 'scholarship' | 'career' | 'activity' | 'rule';
+  year?: number;
+  page?: number;
+  size?: number;
+  sort?: 'asc' | 'desc';
+}
+/** 공지사항 리스트 불러오기 **/
+export const fetchNotices = async ({
+  category,
+  page = 0,
+  size = 15,
+  sort = 'desc',
+  year = new Date().getFullYear(),
+}: {
+  category: string;
+  page?: number;
+  size?: number;
+  sort?: 'asc' | 'desc';
+  year?: number;
+}) => {
+  const { data } = await apiClient.get('/notices', {
+    params: { category, page, size, sort, year },
+  });
+  return data;
+};
--- src/components/organisms/CommonList/index.tsx
+++ src/components/organisms/CommonList/index.tsx
@@ -4,15 +4,17 @@ import DetailItem, { type ListItemProps } from '../DetailItem/idex';
 interface NoticeListProps {
   items: ListItemProps[];
   category: string;
+  page?: number;
+  itemsPerPage?: number;
 }
 
-const CommonList: React.FC<NoticeListProps> = ({ items, category }) => {
+const CommonList: React.FC<NoticeListProps> = ({ items, category, page = 1, itemsPerPage = 8 }) => {
   return (
     <section className='flex flex-col'>
-      {items.map((item) => (
+      {items.map((item, index) => (
         <DetailItem
           key={item.id}
-          id={item.id}
+          id={(page - 1) * itemsPerPage + index + 1}
           category={item.category}
           title={item.title}
           content={item.content}
