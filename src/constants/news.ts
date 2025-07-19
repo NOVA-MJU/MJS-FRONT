b30bf5864cb429c09055a20a857529fc731b5d82
yuraup <o721@naver.com>
2025-07-19 17:43:11 +0900

    <Feat> [#39] 명대신문 페이지 조회 api 연결

--- src/constants/news.ts
+++ src/constants/news.ts
@@ -1,1 +1,10 @@
-export const NewsCategory = ['전체', '보도', '사회'];
+export const NewsCategory: Record<string, string | undefined> = {
+  전체: undefined,
+  리포트: 'REPORT',
+  사회: 'SOCIETY',
+};
+
+export const NewsCategoryLabel: Record<string, string> = {
+  REPORT: '보도',
+  SOCIETY: '사회',
+};
