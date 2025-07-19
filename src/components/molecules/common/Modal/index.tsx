ece7e58345b5f6bcaed6734facc8bd396ce03abc
yuraup <o721@naver.com>
2025-07-20 02:54:33 +0900

    <Feat> 공통 모달 제작

---
+++ src/components/molecules/common/Modal/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title?: string;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <div className='fixed inset-0 z-50 flex items-center justify-center'>
+      <div className='bg-white w-[400px] rounded-2xl shadow-lg p-6 relative border-1 border-grey-10'>
+        {/* 닫기 버튼 */}
+        <button
+          onClick={onClose}
+          className='absolute top-4 right-4 text-gray-400 hover:text-gray-600 text-lg font-bold'
+        >
+          x
+        </button>
+
+        {/* 제목 */}
+        {title && <h2 className='text-xl font-semibold mb-4'>{title}</h2>}
+
+        {/* 컨텐츠 */}
+        <div>{children}</div>
+      </div>
+    </div>,
+    document.body,
+  );
+};
+
+export default Modal;
