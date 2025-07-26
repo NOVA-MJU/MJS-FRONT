71a1a69ee6df8993bd0082e0018ab5e584ff7bd3
yuraup <o721@naver.com>
2025-07-26 15:31:27 +0900

    <Feat> [#43] 마이페이지 닉네임 표시 및 수정페이지 연결 기능 추가

--- src/components/molecules/user/ProfileCard/index.tsx
+++ src/components/molecules/user/ProfileCard/index.tsx
@@ -1,21 +1,25 @@
 import ProfileImage from '../../../atoms/ImageViewer';
 import Button from '../../../atoms/Button';
+import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../../../../store/useAuthStore';
 import { getDepartmentLabel } from '../../../../utils/department';
+// import { useEffect } from 'react';
 
 const ProfileCard = () => {
   const user = useAuthStore((state) => state.user);
   const userDepartmentValue = user?.departmentName || '';
   const departmentLabel = getDepartmentLabel(userDepartmentValue);
 
+  const navigate = useNavigate();
+
   if (!user) return null;
 
   return (
     <div className='bg-white w-full h-[242px] flex justify-center items-center rounded-2xl'>
-      <ProfileImage imageUrl={undefined} width='194px' height='194px' />
+      <ProfileImage imageUrl={user.profileImageUrl} width='194px' height='194px' />
       <div id='contentbox' className='flex flex-col mb-4 gap-4 mt-1 ml-8'>
         <div className='w-78 flex flex-col border-2 border-grey-05 px-4.5 py-4 rounded-xl'>
-          <p className='text-3xl font-medium'>{user.name}</p>
+          <p className='text-3xl font-medium'>{user.nickname}</p>
           <div className='w-full flex gap-2'>
             <p className='text-black'>{departmentLabel}</p>
             <p className='text-grey-40'>|</p>
@@ -23,7 +27,14 @@ const ProfileCard = () => {
           </div>
           <p className='text-grey-40'>{user.email}</p>
         </div>
-        <Button variant='blue35' size='lg' disabled={false} fullWidth={true} shape='rounded'>
+        <Button
+          variant='blue35'
+          size='lg'
+          disabled={false}
+          fullWidth={true}
+          shape='rounded'
+          onClick={() => navigate(`/mypage/${user.uuid}/edit`)}
+        >
           프로필 수정
         </Button>
       </div>
