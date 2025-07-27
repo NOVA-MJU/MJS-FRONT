e41af3e86522fb493b207ed1ff524f4edf6c6ae4
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-07-27 17:13:22 +0900

    <Feat> 명대방송국 메인 페이지 컴포넌트 연동 및 렌더링 완료

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,15 @@
-import NoticeSection from '../../components/organisms/sections/NoticeSection';
-import MealSection from '../../components/organisms/sections/MealSection';
-
-import NewsSection from '../../components/organisms/sections/NewsSection';
+import NoticeSection from '../../components/organisms/Sections/NoticeSection';
+import MealSection from '../../components/organisms/Sections/MealSection';
+import NewsSection from '../../components/organisms/Sections/NewsSection';
 import LayoutForMain from '../../components/templates/LayoutForMain';
+import BroadcastSection from '../../components/organisms/Sections/BroadcastSection';
 const Main = () => {
   return (
     <LayoutForMain>
       <MealSection />
       <NoticeSection />
       <NewsSection />
+      <BroadcastSection />
     </LayoutForMain>
   );
 };
