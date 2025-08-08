6e862dd2448bac493e11d2e4fe968f5c6f53bc99
yuraup <o721@naver.com>
2025-08-08 16:27:33 +0900

    <Style> 레이아웃 버그 수정

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className='flex w-full flex-col md:min-h-screen justify-center items-center md:h-full overflow-hidden'>
       <Navbar />
-      <main className='bg-black flex-1 w-full h-auto md:w-[1280px] mx-auto flex flex-col'>
+      <main className='flex-1 w-full h-auto md:w-[1280px] mx-auto flex flex-col'>
         <Outlet />
       </main>
       <Footer />
