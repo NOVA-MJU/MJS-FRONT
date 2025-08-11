22aae9d52d0b4084ee13499dae9fc8c18651ba1a
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-08-11 21:33:08 +0900

    <Feat> 최상위에 toast 의존성 추가 position : top-center

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { Toaster } from 'react-hot-toast';
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster position='top-center' />
     </QueryClientProvider>
   </BrowserRouter>,
 );
