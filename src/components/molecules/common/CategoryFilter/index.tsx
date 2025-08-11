bae31dfc7c41f48cd0d3ab74111ad796607c164c
yuraup <o721@naver.com>
2025-08-12 01:07:53 +0900

    <Style> [#70] 공지사항 페이지 모바일 반응형 UI 구현

--- src/components/molecules/common/CategoryFilter/index.tsx
+++ src/components/molecules/common/CategoryFilter/index.tsx
@@ -8,21 +8,40 @@ export interface CategoryFilterProps {
 }
 
 const CategoryFilter: React.FC<CategoryFilterProps> = ({ categories, current, onChange }) => (
-  <div className='flex flex-wrap gap-2'>
-    {categories.map((c) => (
-      <Button
-        key={c}
-        variant={current === c ? 'main' : 'greyLight'}
-        shape='pill'
-        size='md'
-        disabled={false}
-        fullWidth={false}
-        onClick={() => onChange(c)}
-      >
-        {c}
-      </Button>
-    ))}
-  </div>
+  <>
+    {/* 모바일 버전  */}
+    <div className='md:hidden flex flex-wrap gap-2'>
+      {categories.map((c) => (
+        <Button
+          key={c}
+          variant={current === c ? 'main' : 'greyLight'}
+          shape='pill'
+          size='sm'
+          disabled={false}
+          fullWidth={false}
+          onClick={() => onChange(c)}
+        >
+          {c}
+        </Button>
+      ))}
+    </div>
+    {/* PC 버전 */}
+    <div className='hidden md:flex flex-wrap gap-2'>
+      {categories.map((c) => (
+        <Button
+          key={c}
+          variant={current === c ? 'main' : 'greyLight'}
+          shape='pill'
+          size='md'
+          disabled={false}
+          fullWidth={false}
+          onClick={() => onChange(c)}
+        >
+          {c}
+        </Button>
+      ))}
+    </div>
+  </>
 );
 
 export default CategoryFilter;
