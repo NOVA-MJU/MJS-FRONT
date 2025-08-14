3f4478d4991e1c3f059760752456ffcf9ef70760
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-08-14 21:12:08 +0900

    <Feat> 모바일 환경에서 공지사항 (FaBullHorn) hidden

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,9 +4,12 @@ export default function Header() {
   return (
     <div className='w-full bg-white'>
       <div className='max-w-screen-xl mx-auto px-4 py-4'>
-        <div className='flex items-center gap-3 text-[1.1rem] font-semibold text-[#0055ff]'>
-          <FaBullhorn className='text-[1.4rem]' />
-          <span>현재 Version1 작업중입니다 _ MJS 일동</span>
+        <div className='hidden'>
+          <div className='flex items-center gap-3 text-[1.1rem] font-semibold text-[#0055ff]'>
+            <FaBullhorn className='text-[1.4rem]' />
+
+            <span>현재 Version1 작업중입니다 _ MJS 일동</span>
+          </div>
         </div>
       </div>
     </div>
