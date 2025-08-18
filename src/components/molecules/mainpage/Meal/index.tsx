0390e8924fb18fde29dc362dece33c0f2c88a159
wooooooooook <otsu9110@naver.com>
2025-08-18 17:20:23 +0900

    <Chore> 메인페이지 학식 스타일 수정

--- src/components/molecules/mainpage/Meal/index.tsx
+++ src/components/molecules/mainpage/Meal/index.tsx
@@ -9,13 +9,12 @@ const MealComponent = ({ title, items, highlight }: MealComponentProps) => {
     <div
       className={` border-grey-20 rounded  ${highlight ? 'bg-white ' : 'bg-white'} shadow-sm w-full `}
     >
-      <div className='border-b  px-4 py-2'>
+      <div className='px-4 py-2'>
         <h2
           className={`text-sm  ${highlight ? 'text-blue-35 ' : 'text-blue-10'} font-semibold text-black`}
         >
           {title}
         </h2>
-        \{' '}
       </div>
       <ul className='px-4 py-2 text-sm text-black-40 space-y-1'>
         {items.map((item, idx) => (
