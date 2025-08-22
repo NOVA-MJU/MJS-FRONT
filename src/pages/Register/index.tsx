49b5e87e0e90c89b49f4b009eaea00222f381c39
yuraup <o721@naver.com>
2025-08-22 10:11:37 +0900

    <Style> 회원가입 헤더 반응형 UI 수정

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -4,7 +4,10 @@ import { Typography } from '../../components/atoms/Typography';
 
 const Register: React.FC = () => (
   <div className='w-full flex-1 bg-grey-05 min-h-screen flex flex-col mx-auto p-4 md:p-12'>
-    <Typography variant='heading01' className='text-mju-primary'>
+    <Typography variant='heading01' className='text-mju-primary hidden md:block'>
+      회원가입
+    </Typography>
+    <Typography variant='heading02' className='text-mju-primary block md:hidden'>
       회원가입
     </Typography>
     <div className='flex justify-center items-center'>
