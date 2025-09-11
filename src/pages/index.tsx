976660eec870f6a5964c150bf14a8515ffd2b714
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2025-09-11 17:16:42 +0900

    <Feat> flex-col (mainLayout) 내부 order-1, order-2 md: order-1 형식으로 디자인 sync 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,19 +4,27 @@ import NoticeSection from '../components/organisms/sections/NoticeSection';
 import NewsSection from '../components/organisms/sections/NewsSection';
 import BroadcastSection from '../components/organisms/sections/BroadcastSection';
 import AcademicCalendar from './academic-calendar';
+
 const Main = () => {
   return (
     <LayoutForMain>
-      <div className='hidden md:block'>
+      <div className='hidden md:block md:order-1'>
         <MealSection />
       </div>
-      <NoticeSection />
-      <NewsSection />
 
-      <div className='hidden md:block'>
+      <div className='order-1 md:order-2'>
+        <NoticeSection />
+      </div>
+
+      <div className='order-3 md:order-3'>
+        <NewsSection />
+      </div>
+
+      <div className='hidden md:block md:order-4'>
         <BroadcastSection />
       </div>
-      <div className='md:block '>
+
+      <div className='order-2 md:hidden'>
         <AcademicCalendar />
       </div>
     </LayoutForMain>
