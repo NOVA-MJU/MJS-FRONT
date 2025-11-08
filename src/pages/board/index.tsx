dc1e002c766d1d765c3f964c49efed1abca1c1d6
wooooooooook <otsu9110@naver.com>
2025-11-08 13:44:02 +1100

    <Chore> board 권한 제한 해제

--- src/pages/board/index.tsx
+++ src/pages/board/index.tsx
@@ -1,17 +1,15 @@
+import { getBoards, type BoardItem } from '@/api/board';
+import Divider from '@/components/atoms/Divider';
+import LoadingIndicator from '@/components/atoms/LoadingIndicator';
+import SearchBar from '@/components/atoms/SearchBar';
+import Pagination from '@/components/molecules/common/Pagination';
+import { useAuthStore } from '@/store/useAuthStore';
+import { formatToElapsedTime } from '@/utils';
+import clsx from 'clsx';
 import { useEffect, useRef, useState } from 'react';
-import SearchBar from '../../components/atoms/SearchBar';
-import { Typography } from '../../components/atoms/Typography';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import Pagination from '../../components/molecules/common/Pagination';
-import { getBoards, type BoardItem } from '../../api/board';
-import LoadingIndicator from '../../components/atoms/LoadingIndicator';
-import Divider from '../../components/atoms/Divider';
 import { IoIosChatbubbles, IoIosHeart } from 'react-icons/io';
 import { RxDividerVertical } from 'react-icons/rx';
-import { formatToElapsedTime } from '../../utils';
-import clsx from 'clsx';
-import toast from 'react-hot-toast';
-import { useAuthStore } from '../../store/useAuthStore';
+import { Link } from 'react-router-dom';
 
 export default function Board() {
   const [contents, setContents] = useState<BoardItem[]>([]);
@@ -19,20 +17,7 @@ export default function Board() {
   const [page, setPage] = useState(0);
   const [totalPages, setTotalPages] = useState(1);
   const initializedRef = useRef(false);
-
-  const isLoggedIn = useAuthStore((s) => s.isLoggedIn);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  /**
-   * 로그인이 되어있지 않은 경우 튕겨냄
-   */
-  useEffect(() => {
-    if (!isLoggedIn) {
-      toast.error('로그인이 필요한 서비스입니다.');
-      navigate('/', { replace: true, state: { from: location.pathname } });
-    }
-  }, [isLoggedIn, navigate, location.pathname]);
+  const { isLoggedIn } = useAuthStore();
 
   /**
    * 페이지네이션 초기화
@@ -40,7 +25,6 @@ export default function Board() {
   useEffect(() => {
     (async () => {
       setIsLoading(true);
-
       try {
         const res = await getBoards(0);
         setContents(res.content);
@@ -75,24 +59,22 @@ export default function Board() {
   return (
     <div className='w-full h-full p-4 md:p-8 flex flex-col gap-4 md:gap-6'>
       <div>
-        <Typography variant='heading01' className='text-mju-primary'>
-          자유게시판
-        </Typography>
+        <h3 className='text-heading01 text-mju-primary'>자유게시판</h3>
       </div>
       <div>
         <SearchBar />
       </div>
       <Divider />
-      <div className='flex justify-end'>
-        <Link
-          className='inline-flex items-center justify-center gap-[10px] p-3 bg-blue-35 rounded-xl m-w-24 md:w-46 cursor-pointer'
-          to={'/board/write'}
-        >
-          <Typography variant='body02' className='text-white'>
-            글 남기기
-          </Typography>
-        </Link>
-      </div>
+      {isLoggedIn && (
+        <div className='flex justify-end'>
+          <Link
+            className='inline-flex items-center justify-center gap-[10px] p-3 bg-blue-35 rounded-xl m-w-24 md:w-46 cursor-pointer'
+            to={'/board/write'}
+          >
+            <span className='text-body02 text-white'>글 남기기</span>
+          </Link>
+        </div>
+      )}
       {isLoading ? (
         <div className='flex justify-center items-center h-screen'>
           <LoadingIndicator />
@@ -147,24 +129,20 @@ function BoardItem({
           isPopular && 'pl-4 border-l-2 border-blue-10',
         )}
       >
-        <Typography variant='body02'>{title}</Typography>
-        <Typography variant='body03' className='line-clamp-1 md:line-clamp-2'>
-          {previewContent}
-        </Typography>
+        <span className='text-body02'>{title}</span>
+        <span className='text-body03 line-clamp-1 md:line-clamp-2'>{previewContent}</span>
         <div className='flex items-center'>
-          <Typography variant='body03' className='text-grey-40 flex gap-1 items-center'>
+          <span className='text-body03 text-grey-40 flex gap-1 items-center'>
             <IoIosHeart />
             {likeCount}
             <RxDividerVertical />
             <IoIosChatbubbles />
             {commentCount}
-          </Typography>
+          </span>
         </div>
       </div>
       <div className='flex items-center justify-center'>
-        <Typography variant='body03' className='text-grey-40'>
-          {formatToElapsedTime(publishedAt)}
-        </Typography>
+        <span className='text-body03 text-grey-40'>{formatToElapsedTime(publishedAt)}</span>
       </div>
     </Link>
   );
