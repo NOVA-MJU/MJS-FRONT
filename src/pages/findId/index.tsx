f426ded682c25bd2a9746e4a5f38265d94b96857
yuraup <o721@naver.com>
2025-11-19 13:50:21 +0900

    <Style> user 관련 로직들 스타일 자동 정렬 적용

--- src/pages/findId/index.tsx
+++ src/pages/findId/index.tsx
@@ -2,11 +2,11 @@ import FindIdForm from '../../components/organisms/FindForm/FindIdForm';
 
 const FindId = () => {
   return (
-    <div className='w-full py-[30%] md:py-10 h-full bg-grey-05 md:w-[1280px] md:min-h-screen flex flex-col md:mx-auto p-12'>
-      <p className='text-2xl mb-4 md:text-4xl font-bold text-mju-primary'>아이디 찾기</p>
-      <div className='w-full mx-auto flex justify-center items-center'>
-        <div className='w-[375px] flex flex-col md:gap-6 md:w-[672px]'>
-          <div className='flex mx-auto w-[80%] md:w-full md:min-h-[480px] items-center bg-white p-6 rounded-xl'>
+    <div className='bg-grey-05 flex h-full w-full flex-col p-12 py-[30%] md:mx-auto md:min-h-screen md:w-[1280px] md:py-10'>
+      <p className='text-mju-primary mb-4 text-2xl font-bold md:text-4xl'>아이디 찾기</p>
+      <div className='mx-auto flex w-full items-center justify-center'>
+        <div className='flex w-[375px] flex-col md:w-[672px] md:gap-6'>
+          <div className='mx-auto flex w-[80%] items-center rounded-xl bg-white p-6 md:min-h-[480px] md:w-full'>
             <FindIdForm />
           </div>
         </div>
