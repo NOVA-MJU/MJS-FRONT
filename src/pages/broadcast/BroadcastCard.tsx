d9f84d9f356f983ab288951e8c4935fc73db7ab7
soyeon <soyeon20031010@gmail.com>
2026-02-06 05:28:30 +0900

    <Fix> 명대뉴스 페이지 오류 방지

--- src/pages/broadcast/BroadcastCard.tsx
+++ src/pages/broadcast/BroadcastCard.tsx
@@ -10,21 +10,30 @@ interface BroadcastCardProps {
 }
 
 export default function BroadcastCard({
-  title,
-  url,
-  playlistTitle,
-  publishedAt,
+  title = '',
+  url = '',
+  playlistTitle = null,
+  publishedAt = '',
 }: BroadcastCardProps) {
+  const youtubeId = typeof url === 'string' ? extractYoutubeId(url) : '';
+  const hasValidEmbed = Boolean(youtubeId);
+
   return (
     <div className='flex flex-1 flex-col gap-4 p-5'>
-      <article key={url} className='flex flex-col'>
-        <iframe
-          className='h-54 w-full rounded-t-[8px] rounded-b-none'
-          src={`https://www.youtube.com/embed/${extractYoutubeId(url)}`}
-          title={title}
-          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-        />
+      <article key={url || 'broadcast'} className='flex flex-col'>
+        {hasValidEmbed ? (
+          <iframe
+            className='h-54 w-full rounded-t-[8px] rounded-b-none'
+            src={`https://www.youtube.com/embed/${youtubeId}`}
+            title={title}
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          />
+        ) : (
+          <div className='border-grey-10 bg-grey-05 flex h-54 w-full items-center justify-center rounded-t-[8px] rounded-b-none border'>
+            <span className='text-caption02 text-grey-40'>영상 링크 없음</span>
+          </div>
+        )}
         <div className='border-grey-10 rounded-t-none rounded-b-[8px] border px-4 py-2'>
           <div className='flex flex-col gap-1'>
             <HighlightedText className='text-body04 text-black'>{title}</HighlightedText>
