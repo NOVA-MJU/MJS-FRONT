bdad7617264cda79478b038fad2fb0709abc04a1
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2026-02-23 21:24:52 +0900

    <Fix> 광고 배너 translate X 삭제 (트랙 밀리는 이슈 해결)

--- src/components/molecules/AdBannerSection.tsx
+++ src/components/molecules/AdBannerSection.tsx
@@ -13,17 +13,17 @@ export default function AdBannerSection() {
   }, [banners.length]);
 
   return (
-    <div className='w-full overflow-hidden'>
-      <div
-        className='flex transition-transform duration-500 ease-out'
-        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
-      >
-        {banners.map((src) => (
-          <div key={src} className='min-w-full shrink-0'>
-            <img src={src} alt='광고 배너' className='h-auto w-full' />
-          </div>
-        ))}
-      </div>
+    <div className='relative h-[clamp(140px,18vh,200px)] w-full overflow-hidden'>
+      {banners.map((src, index) => (
+        <img
+          key={src}
+          src={src}
+          alt='광고 배너'
+          className={`absolute inset-0 h-full w-full transition-opacity duration-700 ${
+            index === activeIndex ? 'opacity-100' : 'opacity-0'
+          }`}
+        />
+      ))}
     </div>
   );
 }
