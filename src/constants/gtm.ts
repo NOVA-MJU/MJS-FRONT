8551f432a54bdeaa3b4293685af8a45f8a5022ec
wooooooooook <otsu9110@gmail.com>
2026-04-09 14:30:37 +0900

    <Chore> 사이드바 멘토관 서비스 접근 버튼 삭제

--- src/constants/gtm.ts
+++ src/constants/gtm.ts
@@ -4,7 +4,7 @@ import type { NavGroup } from '@/types/gtm';
  * 구글애널리틱스 상수
  * SidebarV2 기준으로 nav_group 분류를 통일하기 위한 상수/함수
  * - information: 학과별 정보, 명지도, 공지사항, 학사일정, 학식, 명대신문, 명대뉴스
- * - community: 정보게시판, 자유게시판, 게시판, 멘토관 서비스
+ * - community: 정보게시판, 자유게시판, 게시판
  * - 그 외: etc
  */
 export const NAV_GROUP_INFORMATION_LABELS = new Set([
@@ -17,12 +17,7 @@ export const NAV_GROUP_INFORMATION_LABELS = new Set([
   '명대뉴스',
 ]);
 
-export const NAV_GROUP_COMMUNITY_LABELS = new Set([
-  '정보게시판',
-  '자유게시판',
-  '게시판',
-  '멘토관 서비스',
-]);
+export const NAV_GROUP_COMMUNITY_LABELS = new Set(['정보게시판', '자유게시판', '게시판']);
 
 export function resolveNavGroupByLabel(
   label: string,
