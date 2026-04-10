c2c504115f04553d3e9d5dcff1b1fb73a90501db
WonbinChoi <147716165+ChoiTheCreator@users.noreply.github.com>
2026-04-10 20:57:02 +0900

    <Fix> 탠스택 쿼리 전역 기본 옵션 및 staleTime 상수 추가

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -13,6 +13,16 @@ export const CALENDAR_API_DEFAULT_SIZE = 100;
 export const SEARCH_API_DEFAULT_SIZE = 10;
 
 export const MENU_STALE_TIME_MS = 5 * 60 * 1000;
+export const NOTICE_STALE_TIME_MS = 5 * 60 * 1000;
+export const BOARD_STALE_TIME_MS = 3 * 60 * 1000;
+export const NEWS_STALE_TIME_MS = 5 * 60 * 1000;
+export const BROADCAST_STALE_TIME_MS = 5 * 60 * 1000;
+export const DEPARTMENT_INFO_STALE_TIME_MS = 10 * 60 * 1000;
+export const DEPARTMENT_SCHEDULE_STALE_TIME_MS = 10 * 60 * 1000;
+export const DEPARTMENT_NOTICE_STALE_TIME_MS = 5 * 60 * 1000;
+export const SEARCH_STALE_TIME_MS = 2 * 60 * 1000;
+export const SEARCH_SUGGEST_STALE_TIME_MS = 60 * 1000;
+export const AI_SUMMARY_STALE_TIME_MS = 10 * 60 * 1000;
 
 export const ICON_SIZE_SM = 12;
 export const ICON_SIZE_MD = 16;
